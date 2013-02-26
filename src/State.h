d0037d02b7af4e42303f16e88f7561af4224bc23
LiuJack <462623699@qq.com>
2013-02-26 19:12:06 +0800

    add three statemachine base class

---
+++ src/State.h
@@ -0,0 +1,27 @@
+#ifndef STATE_H
+#define STATE_H
+
+struct Telegram;
+
+template <class entity_type>
+class State
+{
+public:
+
+	virtual ~State(){}
+
+	//this will execute when the state is entered
+	virtual void Enter(entity_type*)=0;
+
+	//this is the states normal update function
+	virtual void Execute(entity_type*)=0;
+
+	//this will execute when the state is exited. 
+	virtual void Exit(entity_type*)=0;
+
+	//this executes if the agent receives a message from the 
+	//message dispatcher
+	virtual bool OnMessage(entity_type*, const Telegram&)=0;
+};
+
+#endif
