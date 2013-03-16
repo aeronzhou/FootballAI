8a8bc14727a050af7a3ec166fd1ed80a73afc6c6
LiuJack <462623699@qq.com>
2013-03-16 21:25:17 +0800

    File architecture change!

---
+++ src/FPGlobalState.h
@@ -0,0 +1,24 @@
+#ifndef FOOTBALL_AI_FIELD_PLAYER_GLOBAL_STATE
+#define FOOTBALL_AI_FIELD_PLAYER_GLOBAL_STATE
+
+#include "State.h"
+
+class FieldPlayer; 
+
+// FieldPlayerGlobalState
+class FieldPlayerGlobalState: public State<FieldPlayer>
+{
+public:
+	static FieldPlayerGlobalState* get();
+
+	void enter(FieldPlayer*) {}
+
+	void execute(FieldPlayer*) {}
+
+	void exit(FieldPlayer*) {}
+
+	bool onMessage(FieldPlayer*, const Message&);
+};
+
+
+#endif
--- src/FieldPlayerState.h
+++ src/FieldPlayerState.h
@@ -1,53 +1,8 @@
 #ifndef FOOTBALL_AI_FIELD_PLAYER_STATE
 #define FOOTBALL_AI_FIELD_PLAYER_STATE
 
-#include "State.h"
-
-class FieldPlayer;
-
-// FieldPlayerGlobalState
-class FieldPlayerGlobalState: public State<FieldPlayer>
-{
-public:
-	static FieldPlayerGlobalState* get();
-
-	void enter(FieldPlayer*) {}
-
-	void execute(FieldPlayer*) {}
-
-	void exit(FieldPlayer*) {}
-
-	bool onMessage(FieldPlayer*, const Message&);
-};
-
-// BackToOrigin
-class BackToOrigin: public State<FieldPlayer>
-{
-public:
-	static BackToOrigin* get();
-
-	void enter(FieldPlayer*);
-
-	void execute(FieldPlayer*);
-
-	void exit(FieldPlayer*);
-
-	bool onMessage(FieldPlayer*, const Message&);
-};
-
-// Waiting
-class Waiting: public State<FieldPlayer>
-{
-public:
-	static Waiting* get();
-
-	void enter(FieldPlayer*);
-
-	void execute(FieldPlayer*);
-
-	void exit(FieldPlayer*);
-
-	bool onMessage(FieldPlayer*, const Message&);
-};
+#include "FPGlobalState.h"
+#include "FPBackToOrigin.h"
+#include "FPWaiting.h"
 
 #endif
